bd67366d9824a1ace8646695a162b6f16edfe23e
stranik28 <jb357506@gmail.com>
2021-11-13 21:40:01 +0300

    add possibility to send data to email

--- js/main.js
+++ js/main.js
@@ -21,10 +21,42 @@ function CheckParams(){
     }
     else{
         $('#send').attr('disabled', 'disabled');
-        console.log("Not all "+$('#check').prop("checked"));
     }
 }
 
-function Send(){
+$(function(){
+    $(".ajaxForm").submit(function(e){
 
-}
+        e.preventDefault();
+
+        var href = $(this).attr("action");
+
+        $.ajax({
+
+            type: "POST",
+
+            dataType: "json",
+
+            url: href,
+
+            data: $(this).serialize(),
+
+            success: function(response){
+
+                if(response.status == "success"){
+
+                    alert("We received your submission, thank you!");
+
+                }else{
+
+                    alert("An error occured: " + response.message);
+
+                }
+
+            }
+
+        });
+
+    });
+
+});
