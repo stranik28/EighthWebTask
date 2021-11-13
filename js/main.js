5e6c465e293f9834b180a1479fb04a62ac1bcfdc
stranik28 <jb357506@gmail.com>
2021-11-13 14:58:05 +0300

    add correct input

--- js/main.js
+++ js/main.js
@@ -2,10 +2,29 @@ $(document).ready(function (){
     PopUpHide();
 });
 
+
 function PopUpShow(){
     $("#pop-up").show();
+    CheckParams();
 }
 
 function PopUpHide(){
     $("#pop-up").hide();
+}
+
+function CheckParams(){
+    let name = $('#name').val();
+    let email = $('#email').val();
+    let mess = $('#mess').val();
+    if(name.length !== 0 && email.length !== 0 && mess.length !== 0 && $('#check').prop("checked")){
+        $('#send').removeAttr('disabled');
+    }
+    else{
+        $('#send').attr('disabled', 'disabled');
+        console.log("Not all "+$('#check').prop("checked"));
+    }
+}
+
+function Send(){
+
 }
